2b21e451fb67781c5df0fdec8b454ae259317a95
natfontanesi <natfontanesi@gmail.com>
2021-04-02 15:56:00 -0300

    ajustando a branch

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from view import userView
 
-print("Insira um termo de pesquisa: ")
+print(
+    "Bem-vindo à API Bitbucket! Com esta API:  \n \
+\n \
+- Acesse informações públicas do usuário;  \n \
+- Acesse repositórios públicos ou privados, tags ou branches;  \n \
+- Crie, atualize ou exclua um de seus repositórios;  \n \
+- Acesse, crie, atualize ou exclua um serviço; \n \
+- Acesse, crie ou exclua uma chave SSH; \n \
+- Baixe um repositório como um arquivo; \n \
+- Acesse, crie, atualize ou exclua um problema; \n \
+- Acesse, crie, atualize ou exclua um comentário. \n \
+ \n \
+Insira um termo de pesquisa: ")
 user = userView()
 user.set_user(input())
+#perguntar em qual formato o usuário deseja a resposta
 print(user.resposta())
